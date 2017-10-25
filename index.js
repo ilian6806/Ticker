714b37cc5564cc23288cc0d0078acc9eab4e3bf1
ilian6806 <ilian6806@gmail.com>
2017-10-26 00:20:21 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+(function() {
+
+    function secondsToTime(sec) {
+
+        var secNum = parseInt(sec, 10);
+        var colon = ':';
+        var days = Math.floor(secNum / 86400);
+        var hours = Math.floor((secNum - days * 86400) / 3600);
+        var minutes = Math.floor((secNum - (hours * 3600) - (days * 86400)) / 60);
+        var seconds = secNum - (days * 86400) - (hours * 3600) - (minutes * 60);
+
+        if (days > 0) { hours += (days * 24); }
+
+        if (hours < 10) { hours = '0' + hours; }
+        if (minutes < 10) { minutes = '0' + minutes; }
+        if (seconds < 10) { seconds = '0' + seconds; }
+
+        return hours + colon + minutes + colon + seconds;
+    }
+
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var tickerSetting = {
+
+        name: 'useTicker',
+        checkBox: document.getElementById('use-it-check'),
+
+        get: function () {
+            return localStorage.getItem(this.name) == 'true';
+        },
+        set: function (value) {
+            var strVal = value ? 'true' : 'false';
+            localStorage.setItem(this.name, strVal);
+            this.checkBox.setAttribute('checked', strVal);
+            this.checkBox.checked = !!value;
+        },
+        init: function () {
+
+            var self = this;
+
+            if (!localStorage.getItem(this.name)) {
+                this.set(false);
+            } else {
+                this.set(this.get());
+            }
+
+            this.checkBox.onchange = function () {
+                self.set(this.checked);
+                window.location.reload();
+            };
+
+            return this;
+        }
+    };
+
+    var isUsed = tickerSetting.init().get();
+
+    var container = document.getElementById('container');
+
+    var ticker = new Ticker();
+    var timers = [];
+
+    // generate random timer times
+    for (var i = 2000; i >= 1; i--) {
+        timers.push(rand(10000, 70000));
+    }
+
+    // ignite timers
+    for (var i = 0, len = timers.length; i < len; i++) {
+
+        var time = timers[i];
+        var timerEL = document.createElement('div');
+        timerEL.className = 'timer';
+        container.appendChild(timerEL);
+
+        (function(tEl, tTime) {
+            if (isUsed) {
+                ticker.set(function () {
+                    tEl.innerHTML = secondsToTime(tTime--);
+                });
+            } else {
+                setInterval(function () {
+                    tEl.innerHTML = secondsToTime(tTime--);
+                }, 1000);
+            }
+        }(timerEL, time));
+    }
+})();
---
+++ src/ticker.js
@@ -0,0 +1,182 @@
+(function (root, factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define('Ticker', factory(root));
+    } else if ( typeof exports === 'object' ) {
+        module.exports = factory(root);
+    } else {
+        root.Ticker = factory(root);
+    }
+})(this, function (root) {
+
+    'use strict';
+
+    // constants
+    var DEFAULT_TIME = 1000;
+
+    // local references
+    var set = root.setInterval;
+    var clear = root.clearInterval;
+
+    // main variables
+    var isTicking = false;
+    var intervalId = null;
+    var actions = {};
+    var lastActionId = 0;
+
+
+    /**
+     * Main constructor
+     */
+    var Ticker = function (time) {
+        this.time = is.number(time) ? time : DEFAULT_TIME;
+    };
+
+
+    /**
+     * Call all active actions on root object
+     */
+    Ticker.prototype.tick = function () {
+        for (var id in actions) {
+            if (actions.hasOwnProperty(id) && is.func(actions[id])) {
+                actions[id].call(root);
+            }
+        }
+    };
+
+
+    /**
+     * Set new action do be executed on tick
+     */
+    Ticker.prototype.set = function (func) {
+
+        if (is.not.func(func)) {
+            return showError(new ArgumentException('First argument must be of type function.'));
+        }
+
+        actions[++lastActionId] = func;
+
+        if (! isTicking) {
+            this.start();
+        }
+
+        return lastActionId;
+    };
+
+
+    /**
+     * Remove action by id from execution queue
+     */
+    Ticker.prototype.clear = function (id) {
+
+        if (is.not.number(id)) {
+            return showError(new ArgumentException('First argument must be of type integer.'));
+        }
+
+        var success = !!actions.hasOwnProperty(id);
+        delete actions[id];
+
+        if (! Object.keys(actions).length) {
+            this.stop();
+        }
+
+        return success;
+    };
+
+
+    /**
+     * Remove all active actions
+     */
+    Ticker.prototype.clearAll = function () {
+        actions = {};
+        this.stop();
+    };
+
+
+    /**
+     * Start ticker
+     */
+    Ticker.prototype.start = function () {
+
+        if (isTicking) {
+            return;
+        }
+
+        intervalId = set.call(root, this.tick, this.time);
+        isTicking = true;
+
+        return intervalId;
+    };
+
+
+    /**
+     * Stop ticker
+     */
+    Ticker.prototype.stop = function () {
+
+        clear(intervalId);
+        isTicking = false;
+
+        return intervalId;
+    };
+
+
+    /**
+     * Basic type checker
+     */
+    var is = {
+        number: function (p) {
+            return (typeof p).toLowerCase() === 'number';
+        },
+        string: function (p) {
+            return (typeof p).toLowerCase() === 'string';
+        },
+        array: function (p) {
+            return p instanceof Array;
+        },
+        object: function (p) {
+            return (typeof p).toLowerCase() === 'object' && p.constructor === Object;
+        },
+        func: function (p) {
+            return !!(p && p.constructor && p.call && p.apply);
+        },
+        htmlElement: function (p) {
+            return p instanceof HTMLElement;
+        },
+        undefined: function (p) {
+            return [undefined, null, 'undefined', 'null'].indexOf(p) > -1;
+        },
+        not: {}
+    };
+
+    // add negative checks
+    for (var type in is) {
+        if (is.hasOwnProperty(type) && type !== 'not') {
+            is.not[type] = new Function('p', 'return ! (' + is[type] + ')(p)');
+        }
+    }
+
+
+    /**
+     * Exceptions
+     */
+    function showError(e) {
+        console.error(
+            '[Ticker]' +
+            (
+                (e.constructor && e.constructor.name)
+                    ? '[' + e.constructor.name + ']'
+                    : ''
+            ) +
+            ': ' + e.message
+        );
+        if (e.stack && e.stack.split) {
+            console.log(e.stack.split('\n').slice(1).join('\n'));
+        }
+    }
+
+    function ArgumentException(m) {
+        this.message = m;
+    }
+
+    return Ticker;
+});
